4b23241ffea136eba584c31fb12c599891607aa6
jeongsam3659 <57051148+jeongsam3659@users.noreply.github.com>
2021-12-22 18:59:56 +0900

    Js-update
    랜덤 최대범위과
    
    플레이어 수 비교
    
    //quest1 고민해보기

---
+++ JS_3/script.js
@@ -0,0 +1,47 @@
+const rsForm = document.getElementById("js-form");
+const rsResult = document.getElementById("js-result");
+const maxNum = document.getElementById("maxNumber");
+
+function actionRandomNum(e) {
+    e.preventDefault();
+    const inPlayerNum = rsForm.querySelector("input");
+    //input칸이 비었을 경우
+    if (maxNum.value === "") {
+        maxNum.focus();
+        console.log("최대 범위 input칸이 비어있습니다.");
+        return;
+        // Quest1
+        if (inPlayerNum.value === "") {
+            inPlayerNum.focus();
+            console.log("플레이어 input칸이 비어있습니다.");
+            return;
+        }
+    }
+
+    const max = maxNum.value;
+    //랜덤 수(올림)
+    const random = Math.ceil(Math.random() * max);
+    console.log("random값 : ", random);
+    //플레이어 수를 10진수화
+    const userNum = parseInt(inPlayerNum.value, 10);
+    console.log("플레이값 : ", userNum);
+    //결과 태그
+    const resultSpan = rsResult.querySelector("#html");
+
+    resultSpan.innerHTML = `
+    당신의 선택은 : ${userNum}, <br/>
+    랜덤 수 값: ${random}. <br/>
+    <strong>
+        ${userNum === random ? "정답!" : "떙!"}
+    </strong>
+    `;
+}
+
+rsForm.addEventListener("submit", actionRandomNum);
+
+/*
+    Quest1
+    최대범위의 수가 비어있을때는 console.log()와 focus가 정상적으로 작동하는데
+    최대범위의수가 있는 상태에서 플레이어예상수(inputNum)이 비어있을때 action이 작동하지 않느다.
+
+*/
