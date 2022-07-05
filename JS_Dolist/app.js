8fb45073a5effe0dc33558a089e02fbc973c235c
jeongsam3659 <57051148+jeongsam3659@users.noreply.github.com>
2022-07-06 01:55:50 +0900

    doList
    input 기

---
+++ JS_Dolist/app.js
@@ -0,0 +1,9 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+    console.log(loginInput.value);
+    console.log("click!!");
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
